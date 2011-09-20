5fb2faeadd867d39ac98d0aeea43ce2b15d63749
Hajime Hoshi <hajimehoshi@gmail.com>
2011-09-20 16:52:21 -0700

    refactoring

--- 1.hs
+++ 1.hs
@@ -1,5 +1,7 @@
+main :: IO ()
+main = putStrLn $ show result
+  where
+    result = sum nums
+
 nums :: [Int]
 nums = [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
-
-main :: IO ()
-main = putStrLn $ show $ sum nums
--- 2.hs
+++ 2.hs
@@ -1,5 +1,7 @@
+main :: IO ()
+main = putStrLn $ show result
+  where
+    result = sum $ filter even $ takeWhile (\x -> x < 4000000) fibs
+
 fibs :: [Integer]
 fibs = 1 : 2 : zipWith (+) fibs (tail fibs)
-
-main :: IO ()
-main = putStrLn $ show $ sum $ filter even $ takeWhile (\x -> x < 4000000) fibs
