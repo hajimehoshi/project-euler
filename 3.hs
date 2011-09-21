21417ac4bfb658b89bfc383f9b42aa0dd80eeb67
Hajime Hoshi <hajimehoshi@gmail.com>
2011-09-20 17:19:08 -0700

    refactoring

--- 3.hs
+++ 3.hs
@@ -10,10 +10,10 @@ mainNum = 600851475143
 factors :: Integer -> [Integer]
 factors num = factors' num psuedoPrimes
   where
+    factors' _    []            = error "invalid primes"
     factors' num' primes@(p:ps)
       | num' == 1         = []
       | num' < p * p      = [num']
       | num' `mod` p == 0 = p : factors' (num' `div` p) primes
-      | otherwise        = factors' num' ps
-    factors' _ [] = error "invalid primes"
+      | otherwise         = factors' num' ps
     psuedoPrimes = 2:[3,5..]
