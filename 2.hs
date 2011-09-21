55cac406b7495025f5939c05fa7b2b1b67f6c425
Hajime Hoshi <hajimehoshi@gmail.com>
2011-09-20 18:57:20 -0700

    refactoring 2.hs

--- 2.hs
+++ 2.hs
@@ -1,7 +1,7 @@
 main :: IO ()
 main = putStrLn $ show result
   where
-    result = sum $ filter even $ takeWhile (\x -> x < 4000000) fibs
+    result = sum $ filter even $ takeWhile (< 4000000) fibs
 
 fibs :: [Integer]
 fibs = 1 : 2 : zipWith (+) fibs (tail fibs)
