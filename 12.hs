90aa558d7ebfd6a9835a87c6dbd49405042397d2
Hajime Hoshi <hajimehoshi@gmail.com>
2011-09-22 01:36:44 -0700

    added 12.hs

---
+++ 12.hs
@@ -0,0 +1,24 @@
+import Data.List
+
+main :: IO ()
+main = putStrLn $ show result
+
+result :: Integer
+result = head $ dropWhile ((<500) . nDivisor) triangleNumbers
+
+triangleNumbers :: [Integer]
+triangleNumbers = scanl1 (+) [1..]
+               
+nDivisor :: Integer -> Integer
+nDivisor num = product $ map ((+1) . toInteger . length) $ group $ factors num
+
+factors :: Integer -> [Integer]
+factors num = factors' num psuedoPrimes
+  where
+    factors' _    []            = error "invalid primes"
+    factors' num' primes@(p:ps)
+      | num' == 1         = []
+      | num' < p * p      = [num']
+      | num' `mod` p == 0 = p : factors' (num' `div` p) primes
+      | otherwise         = factors' num' ps
+    psuedoPrimes = 2:[3,5..]
