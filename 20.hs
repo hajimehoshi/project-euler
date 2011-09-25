62c89b2a764dd42323fe26cc40f0076c5d9394a8
Hajime Hoshi <hajimehoshi@gmail.com>
2011-09-25 03:37:02 -0700

    added 20.hs

---
+++ 20.hs
@@ -0,0 +1,12 @@
+import Data.Char (digitToInt)
+
+main :: IO ()
+main = putStrLn $ show result
+
+result :: Integer
+result = sum $ map (toInteger . digitToInt) $ show $ factorial 100
+
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial 1 = 1
+factorial n = n * (factorial (n-1))
